b55503dd10e5923dc540eeaab895547bf909f5ba
Beata Ariel <Beata Ariel@192.168.0.4>
2019-09-04 21:20:06 +0530

    comments added

--- SampleProject/src/com/defi/main/HelloWorld.java
+++ SampleProject/src/com/defi/main/HelloWorld.java
@@ -4,9 +4,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		// Added comments
 		System.out.println("Hello World");
 		
+			
 	}
 
 }
